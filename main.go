2682004e885d006aa8bd0027aefe402190e5cf18
Shubh <shubh.scb@gmail.com>
2024-03-15 17:43:11 +0530

    feat: project initiate go + Fiber

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+    // Create a new Fiber instance
+    app := fiber.New()
+
+    // Define routes
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    // Start the server
+    app.Listen(":3000")
+}
